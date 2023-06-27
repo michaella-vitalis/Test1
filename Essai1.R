78ee3de2aa81934e6b9c82247aaf2e40e60e1f72
michaella-vitalis <137802858+michaella-vitalis@users.noreply.github.com>
2023-06-27 11:39:12 +0200

    Add files via upload
    fonction weighted.mean2

---
+++ Essai1.R
@@ -0,0 +1,25 @@
+#coucou Nicolas !!!
+
+weighted.mean2 <- function(group.mean, pourcent_pop, pourcent_gp) {
+  
+  # variable
+  
+  weight <- pourcent_pop/pourcent_gp
+  N <- length(weight[weight != 0])
+  
+  # mean
+  
+  weighted_mean <- sum(group.mean * weight)/(sum(weight))
+  
+  # standard deviation
+  
+  weighted_standard_deviation <- sqrt(sum(weight * (group.mean - weighted_mean) ^ 2) / (((N - 1) / N) * sum(group.mean)))
+  
+  return(
+    list("weighted mean" = weighted_mean,
+         "weighted standard deviation " = weighted_standard_deviation
+    )
+  )
+}
+
+
